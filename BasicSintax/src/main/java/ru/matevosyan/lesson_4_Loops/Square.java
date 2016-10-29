3c00001140878f080f8a882c941ee8f902d22bb0
VardanMatevosyan <vardya@yandex.ru>
2016-10-29 15:45:01 +0300

    add Square.java

--- BasicSintax/src/main/java/ru/matevosyan/lesson_4_Loops/Square.java
+++ BasicSintax/src/main/java/ru/matevosyan/lesson_4_Loops/Square.java
@@ -16,17 +16,15 @@ public class Square{
 
 	public  float calculate(int x) {
 	
-		return (float)(a*Math.pow(x,2)) + (b * x) + c;
+		return (float)(this.a*Math.pow(x,2)) + (this.b * x) + this.c;
 	
 	}
 
 	public void show(int start, int finish, int step) {
 		
-		Square s = new Square(1f,2f,3f);
-
 		while(start < finish) {
 
-			System.out.println(s.calculate(start));
+			System.out.println(this.calculate(start));
 			start += step;
 
 		}

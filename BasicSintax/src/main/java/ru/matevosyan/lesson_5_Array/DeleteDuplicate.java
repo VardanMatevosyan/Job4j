1a992df2953a33485b40c779a3e9bd55d99825a4
VardanMatevosyan <vardya@yandex.ru>
2016-10-29 15:23:14 +0300

    add DeleteDuplicate.java

---
+++ BasicSintax/src/main/java/ru/matevosyan/lesson_5_Array/DeleteDuplicate.java
@@ -0,0 +1,33 @@
+package ru.matevosyan.lesson_5_Array;
+
+public class DeleteDuplicate{
+
+	public String[] searchDuplicate (String mas[]) {
+
+		int j = 0;		
+
+		for (int k = mas.length-1; k > 0; k--) {
+							
+			for (int i = 1; i < mas.length - 1; i++) {
+			
+				if (mas[j].equals(mas[i+1])) {
+				
+					mas[i+1] = null;
+
+					if (i == mas.length -1) {
+
+						j += 1;
+
+					}
+							
+				}
+													
+			}
+
+		}
+
+	return mas;
+
+	}
+
+}

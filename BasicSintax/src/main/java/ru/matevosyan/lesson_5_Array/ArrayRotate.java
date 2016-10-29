aa1ea5557b5a7241a68c570eb05399741799742e
VardanMatevosyan <vardya@yandex.ru>
2016-10-29 15:23:47 +0300

    add ArrayRotate.java

--- BasicSintax/src/main/java/ru/matevosyan/lesson_5_Array/ArrayRotate.java
+++ BasicSintax/src/main/java/ru/matevosyan/lesson_5_Array/ArrayRotate.java
@@ -2,21 +2,18 @@ package ru.matevosyan.lesson_5_Array;
 
 public class ArrayRotate {
 
-	private int MaxSize = 4;
-	private int lastPosition = MaxSize - 1;
-	private int level = 0;
-	private int totLevel = MaxSize / 2;
 	
 	public int[][] rotateMatrix(int value[][]){
+
+
+	int MaxSize = 4;
+	int lastPosition = MaxSize - 1;
+	int level = 0;
+	int totLevel = MaxSize / 2;
 	
 		while (level < totLevel) {
 				
 			for (int i = level; i < lastPosition; i++) {
-
-					
-				//this.swap(value[level][i], value[i][lastPosition]);
-				//this.swap(value[level][i], value[lastPosition][lastPosition - i + level]);
-				//this.swap(value[level][i], value[lastPosition - i + level][level]);
 
 				this.swap(value, level, i, i, lastPosition);
 				this.swap(value, level, i, lastPosition, lastPosition - i + level);
@@ -32,13 +29,6 @@ public class ArrayRotate {
 	
 	}
 
-	//public void swap(int i, int j){
-
-		//int current = i;
-		//i = j;
-		//j = current;
-	
-	//}
 	public void swap(int arr[][], int i, int j, int a, int b){
 
 		int current = arr[i][j];

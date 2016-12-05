7547b17df391feb595808ff152f74bb5377ebf1d
VardanMatevosyan <vardya@yandex.ru>
2016-12-05 23:14:09 +0300

    Add package-info.java

---
+++ OOP/Tracker/src/test/java/ru/matevosyan/models/package-info.java
@@ -0,0 +1,7 @@
+/**Created for testing all classes that included in model package
+ * Created on 05.12.2016.
+ * @since 1.0
+ * @version 1.0
+ * @author Matevosyan Vardan
+ */
+package ru.matevosyan.models;

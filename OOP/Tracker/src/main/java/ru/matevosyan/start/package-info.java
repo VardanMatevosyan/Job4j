b8bab828f9949f673305d8f62d2ea7aaf4503504
VardanMatevosyan <vardya@yandex.ru>
2016-12-05 23:11:51 +0300

    Add package-info.java

---
+++ OOP/Tracker/src/main/java/ru/matevosyan/start/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package was created for starting the program and save all the items that have been created.
+ * Created on 04.12.2016.
+ * @author Matevosyan Vardan
+ * @since 1.0
+ * @version 1.0
+ */
+
+package ru.matevosyan.start;

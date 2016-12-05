472b317fe21a15817f5e58bca957e94fec513fc8
VardanMatevosyan <vardya@yandex.ru>
2016-12-05 23:10:31 +0300

    Add package-info.java

---
+++ OOP/Tracker/src/main/java/ru/matevosyan/models/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package created for determine the type of models in our program.
+ * Created on 04.12.2016.
+ * @author Matevosyan Vardan
+ * @since 1.0
+ * @version 1.0
+ */
+
+package ru.matevosyan.models;

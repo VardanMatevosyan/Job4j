bb204863567b4ce1fa14f85138a1e6453967b542
VardanMatevosyan <vardya@yandex.ru>
2016-12-05 23:12:25 +0300

    Add StartUI.java

---
+++ OOP/Tracker/src/main/java/ru/matevosyan/start/StartUI.java
@@ -0,0 +1,28 @@
+package ru.matevosyan.start;
+
+/**
+ * Created class StartUI for starting the program.
+ * Created on 25.11.2016.
+ * @since 1.0
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
+public class StartUI {
+
+    /**
+     * The static main method which is starting our program.
+     * @param args array of String elements
+     */
+
+    public static void main(String[] args) {
+
+        /**
+         * Instance variable tracker.
+         */
+
+        Tracker tracker = new Tracker();
+
+    }
+
+}

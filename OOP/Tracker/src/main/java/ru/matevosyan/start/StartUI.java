b3f5f305531af2c99310649f9807e5b722fd5423
VardanMatevosyan <vardya@yandex.ru>
2016-12-06 23:32:12 +0300

    update pom.xml: "add manifest for execute jar file"

--- OOP/Tracker/src/main/java/ru/matevosyan/start/StartUI.java
+++ OOP/Tracker/src/main/java/ru/matevosyan/start/StartUI.java
@@ -1,5 +1,8 @@
 package ru.matevosyan.start;
 
+import ru.matevosyan.models.Item;
+import ru.matevosyan.models.Task;
+
 /**
  * Created class StartUI for starting the program.
  * Created on 25.11.2016.
@@ -22,6 +25,12 @@ public class StartUI {
          */
 
         Tracker tracker = new Tracker();
+        tracker.add(new Task("TaskName", "TaskDesk"));
+        tracker.add(new Task("TaskName2", "TaskDesk"));
+        tracker.add(new Item("TaskName3", "TaskDesk"));
+        for (Item item : tracker.getAll()) {
+            System.out.println(item);
+        }
 
     }
 

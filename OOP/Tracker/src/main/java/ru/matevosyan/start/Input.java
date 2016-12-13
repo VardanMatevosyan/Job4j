692d7c3fbab5489e89d397de8546b9d05d2ead37
VardanMatevosyan <vardya@yandex.ru>
2016-12-13 07:10:59 +0300

    add interface Input.java

---
+++ OOP/Tracker/src/main/java/ru/matevosyan/start/Input.java
@@ -0,0 +1,13 @@
+package ru.matevosyan.start;
+
+/**
+ * Interface Input.
+ * Created on 07.12.2016.
+ * @since 1.0
+ * @version 1.0
+ * @author Matevosyan Vardan
+ */
+
+public interface Input {
+    String ask(String question);
+}

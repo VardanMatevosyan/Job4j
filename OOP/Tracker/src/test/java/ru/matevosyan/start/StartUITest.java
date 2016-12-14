73ceb305f09d33768ebee7743e5162a5a96955ae
VardanMatevosyan <vardya@yandex.ru>
2016-12-14 07:31:44 +0300

    add StartUITest.java

--- OOP/Tracker/src/test/java/ru/matevosyan/start/StartUITest.java
+++ OOP/Tracker/src/test/java/ru/matevosyan/start/StartUITest.java
@@ -14,8 +14,9 @@ import static org.junit.Assert.*;
  */
 
 public class StartUITest {
+
     @Test
-    public void whenCreateItemThenSaveInTracker() {
+    public void whenCreateItemThenFindByNameInTracker() {
 
         String[] answers = {
 
@@ -27,9 +28,87 @@ public class StartUITest {
 
         Input stub = new StubInput(answers);
         Tracker tracker = new Tracker();
-
         new StartUI(stub, tracker).init();
         Item item = tracker.findByName("task 1");
+
         assertThat(item.getName(), is("task 1"));
+    }
+
+    @Test
+    public void whenCreateItemThenFindByIdTracker() {
+
+        String[] answer = {
+                "1",
+                "task 2",
+                "task desc",
+                "n"
+        };
+
+        Input stub = new StubInput(answer);
+        Tracker tracker = new Tracker();
+        new StartUI(stub, tracker).init();
+
+        for (Item item : tracker.getAll()){
+            item = tracker.findById(item.getId());
+
+            assertThat(item, is(tracker.getAll()[0]));
+        }
+
+
+    }
+
+    @Test
+    public void whenCreateItemThenFindByDateTracker() {
+
+        String[] answer = {
+                "1",
+                "task 3",
+                "task desc",
+                "n"
+        };
+
+        Input stub = new StubInput(answer);
+        Tracker tracker = new Tracker();
+        new StartUI(stub, tracker).init();
+
+        for (Item item : tracker.getAll()){
+            item = tracker.findByDate(item.getCreate());
+
+            assertThat(item, is(tracker.getAll()[0]));
+        }
+
+
+    }
+
+    @Test
+    public void whenCreateItemThenEditTracker() {
+
+        String id = "0";
+        Tracker tracker = new Tracker();
+
+        for (Item item : tracker.getAll()){
+            id = item.getId();
+        }
+
+        String[] answer = {
+                "1",
+                "task 3",
+                "task desc",
+                "y",
+                "2",
+                id,
+                "task 4",
+                "task desc",
+                "n"
+        };
+
+        Input stub = new StubInput(answer);
+        new StartUI(stub, tracker).init();
+
+        for (Item item : tracker.getAll()){
+           assertThat(item.getName(), is("task 4"));
+        }
+
+
     }
 }

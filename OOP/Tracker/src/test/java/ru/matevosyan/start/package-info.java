fe2cab0c91ea59b50350e6720183d5ca97cdb96b
VardanMatevosyan <vardya@yandex.ru>
2016-12-18 22:01:25 +0300

    add package-info.java

---
+++ OOP/Tracker/src/test/java/ru/matevosyan/start/package-info.java
@@ -0,0 +1,8 @@
+/**Created for testing all classes that included in start package
+ * Created on 05.12.2016.
+ * @since 1.0
+ * @version 1.0
+ * @author Matevosyan Vardan
+ */
+
+package ru.matevosyan.start;

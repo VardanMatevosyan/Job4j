fa5d3050fc7d19e290bec3a5caec988c9f56112a
VardanMatevosyan <vardya@yandex.ru>
2016-12-27 23:17:08 +0300

    override method ask

--- OOP/Tracker/src/main/java/ru/matevosyan/start/StubInput.java
+++ OOP/Tracker/src/main/java/ru/matevosyan/start/StubInput.java
@@ -41,4 +41,10 @@ public class StubInput implements Input {
     public String ask(String question) {
         return answers[position++];
     }
+
+    @Override
+    public int ask(String question, int[] range) {
+       // throw new UnsupportedOperationException("UnsupportedOperationException");
+    return -1;
+    }
 }

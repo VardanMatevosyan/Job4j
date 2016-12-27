b6ed8ccb22fc1cbf99992ffefcd9b7dd1ca3dd91
VardanMatevosyan <vardya@yandex.ru>
2016-12-27 23:09:32 +0300

    add MenuOutException.java for throw exception

---
+++ OOP/Tracker/src/main/java/ru/matevosyan/start/MenuOutException.java
@@ -0,0 +1,16 @@
+package ru.matevosyan.start;
+
+/**
+ * Created for valid input data.
+ * Created on 27.12.2016.
+ * @since 1.0
+ * @version 1.0
+ * @author Matevosyan Vardan
+ */
+
+public class MenuOutException extends RuntimeException {
+
+    public MenuOutException(String msg) {
+        super(msg);
+    }
+}

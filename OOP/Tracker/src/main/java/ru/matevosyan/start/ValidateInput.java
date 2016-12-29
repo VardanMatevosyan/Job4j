5fb0916aa90f7e5e31e6a373b5e40862885c11c0
VardanMatevosyan <vardya@yandex.ru>
2016-12-29 07:34:59 +0300

    add ValidateInput

--- OOP/Tracker/src/main/java/ru/matevosyan/start/ValidateInput.java
+++ OOP/Tracker/src/main/java/ru/matevosyan/start/ValidateInput.java
@@ -21,12 +21,12 @@ public class ValidateInput extends ConsoleInput {
                 invalid = false;
             } catch (MenuOutException moe) {
                 System.out.println("Please select key from menu");
-            }
-            catch (NumberFormatException nfe) {
+            } catch (NumberFormatException nfe) {
                 System.out.println("Please enter validate data again");
             }
         } while (invalid);
+
         return value;
-        }
     }
+}
 

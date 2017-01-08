0424102899de961ccdc87a3ec5aa446718699571
VardanMatevosyan <vardya@yandex.ru>
2017-01-08 19:22:31 +0300

    add BaseAction.java to override key and info methods in this class

---
+++ OOP/Tracker/src/main/java/ru/matevosyan/start/BaseAction.java
@@ -0,0 +1,33 @@
+package ru.matevosyan.start;
+
+/**
+ * Interface UserAction.
+ * Created on 08.01.2017.
+ * @since 1.0
+ * @version 1.0
+ * @author Matevosyan Vardan
+ */
+
+abstract public  class BaseAction implements UserAction {
+
+    private int key;
+    private String name;
+
+    public BaseAction(int key, String name) {
+        this.name = name;
+        this.key = key;
+    }
+
+    @Override
+    public int key(){
+        return this.key;
+    }
+
+    public abstract void execute(Input input, Tracker tracker);
+
+    @Override
+    public String info() {
+        return String.format("%s. %s", this.key, this.name);
+    }
+
+}

6def1faec70b33a4cd226fa19c647e56f8873ec2
VardanMatevosyan <vardya@yandex.ru>
2017-01-26 00:31:15 +0300

    create package-info to explain what we do in this package

---
+++ OOP/ControlArrayWork/src/main/java/ru/matevosyan/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created BiggerRepeatValue fo finding bigger repeating number in array.
+ * Created on 25.01.2017.
+ * @since 1.0
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
+package ru.matevosyan;

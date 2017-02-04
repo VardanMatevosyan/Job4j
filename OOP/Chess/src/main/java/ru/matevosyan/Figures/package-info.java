e18f291969dbe12619856a98458d05d8eaa4634d
VardanMatevosyan <vardya@yandex.ru>
2017-02-04 12:36:22 +0300

    add package-info to Figure package

---
+++ OOP/Chess/src/main/java/ru/matevosyan/Figures/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created models to hold all chess-project figures.
+ * Created on 04.02.2017.
+ * @since 1.0
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+package ru.matevosyan.Figures;

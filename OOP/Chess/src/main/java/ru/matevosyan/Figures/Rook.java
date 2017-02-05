ae0f45017c5747db4f8827620974158df2d5592d
VardanMatevosyan <vardya@yandex.ru>
2017-02-05 18:53:43 +0300

    Create new Rook class and implement way method which return the rook movement array

---
+++ OOP/Chess/src/main/java/ru/matevosyan/Figures/Rook.java
@@ -0,0 +1,106 @@
+package ru.matevosyan.Figures;
+
+import ru.matevosyan.exceptions.FigureNotFoundException;
+import ru.matevosyan.exceptions.ImpossibleMoveException;
+import ru.matevosyan.models.Cell;
+import ru.matevosyan.models.Figure;
+
+/**
+ * Created Rook as one of figures.
+ * Created on 29.01.2017.
+ * @since 1.0
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
+public class Rook extends Figure {
+
+    /**
+     * Rook name.
+     */
+
+    private final String name = "Rook";
+
+    /**
+     * Rook constructor invoke parent constructor which assign Cell position.
+     * @param position Rook position.
+     */
+
+    public Rook(Cell position) {
+        super(position);
+    }
+
+    /**
+     * Method way find the rook movement and assign to Cell array which is returning in the end.
+     * And we get all the way that bishop is getting.
+     * @param dist the way that we want to get.
+     * @return rookWay that is actually array of cell or array of movement.
+     * @throws ImpossibleMoveException throw when move is imposable.
+     */
+
+    @Override
+    public Cell[] way(Cell dist) throws ImpossibleMoveException {
+
+        Cell[] rookWay = new Cell[7];
+        Cell cellRook = new Cell();
+        int i = 0;
+
+        /**
+         * Get Cell distance X and Y getting from passing to method way.
+         */
+
+        int distX = dist.getX();
+        int distY = dist.getY();
+
+        /**
+         * Get Cell current position X and Y getting from passing to method way.
+         */
+
+        int currentDistX = this.position.getX();
+        int currentDistY = this.position.getY();
+
+        if (distY > currentDistY & distX == currentDistX) {
+            cellRook.setX(currentDistX);
+            do {
+                cellRook.setY(++currentDistY);
+                rookWay[i++] = cellRook;
+            } while (currentDistY != distY);
+        } else {
+            cellRook.setX(currentDistX);
+            do {
+                cellRook.setY(--currentDistY);
+                rookWay[i++] = cellRook;
+            } while (currentDistY != distY);
+        }
+
+        if (distX > currentDistX) {
+            cellRook.setY(currentDistY);
+            do {
+                cellRook.setX(++currentDistX);
+                rookWay[i++] = cellRook;
+            } while (currentDistX != distX);
+        } else {
+            cellRook.setY(currentDistY);
+            do {
+                cellRook.setX(--currentDistX);
+                rookWay[i++] = cellRook;
+            } while (currentDistX != distX);
+        }
+
+        return rookWay;
+    }
+
+    /**
+     * Override clone method in Figure class which is actually abstract.
+     * @param destination the way that we want to get.
+     * @return new rook instance.
+     * @throws FigureNotFoundException that is men that clone can not find figure in destination position.
+     */
+
+    @Override
+    public Figure clone(Cell destination) throws FigureNotFoundException {
+        return new Rook(destination);
+    }
+
+
+}

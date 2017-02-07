6c1920ccd3d38c072ffc8f2f00385aad3c456799
VardanMatevosyan <vardya@yandex.ru>
2017-02-07 07:28:32 +0300

    add package-info

---
+++ OOP/Chess/src/test/java/ru/matevosyan/Figures/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created testing Bishop class for finding right bishop way in Cell array.
+ * Created on 25.01.2017.
+ * @since 1.0
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
+package ru.matevosyan.Figures;

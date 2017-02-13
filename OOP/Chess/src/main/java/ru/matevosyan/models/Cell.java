d6e27f9a198fe8987a7884eca48cf68db2598cf6
VardanMatevosyan <vardya@yandex.ru>
2017-02-13 14:09:59 +0300

    refactoring Cell

--- OOP/Chess/src/main/java/ru/matevosyan/models/Cell.java
+++ OOP/Chess/src/main/java/ru/matevosyan/models/Cell.java
@@ -10,26 +10,68 @@ package ru.matevosyan.models;
 
 public class Cell {
 
+    /**
+     * x first cell coordinate which is row.
+     */
+
     private int x;
+
+    /**
+     * y second cell coordinate which is column.
+     */
+
     private int y;
+
+    /**
+     * Cell constructor which defined cell coordinates.
+     * @param x x first cell coordinate which is row and passing to constructor.
+     * @param y y second cell coordinate which is column and passing to constructor.
+     */
 
     public Cell(int x,  int y) {
         this.x = x;
         this.y = y;
     }
-    public Cell() {}
+
+    /**
+     * Constructor by default.
+     */
+
+    public Cell() {
+
+    }
+
+    /**
+     * Getter for first coordinate.
+     * @return x is first coordinate.
+     */
 
     public int getX() {
         return x;
     }
 
+    /**
+     * Getter for second coordinate.
+     * @return y is second coordinate.
+     */
+
     public int getY() {
         return y;
     }
 
+    /**
+     * Setter setX assign to current variable this.x passing variable x.
+     * @param x is passing variable x.
+     */
+
     public void setX(int x) {
         this.x = x;
     }
+
+    /**
+     * Setter setY assign to current variable this.y passing variable y.
+     * @param y is passing variable y.
+     */
 
     public void setY(int y) {
         this.y = y;

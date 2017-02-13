2660ff031e0913fc838251cb3d53a9edc62048b5
VardanMatevosyan <vardya@yandex.ru>
2017-02-13 07:26:18 +0300

    add package-info

---
+++ IO/InputOutput/src/main/java/ru/matevosyan/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created for checking even and odd numbers and get true or false.
+ * Created on 09.02.2017.
+ * @since 1.0
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
+package ru.matevosyan;

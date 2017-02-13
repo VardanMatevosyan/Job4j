b448c95e6d5091a57d3d899ec8a9ec0205165086
VardanMatevosyan <vardya@yandex.ru>
2017-02-13 07:27:44 +0300

    add EvenAndOddNumberTest for testing EvenAndOddNumber class

---
+++ IO/InputOutput/src/test/java/ru/matevosyan/EvenAndOddNumberTest.java
@@ -0,0 +1,61 @@
+package ru.matevosyan;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created for testing EvenAndOddNumber class
+ * Created on 09.02.2017.
+ * @since 1.0
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
+public class EvenAndOddNumberTest {
+
+    /**
+     * whenNumberIsEvenThanReturnTrue test situation when number is even.
+     * @throws IOException
+     */
+
+    @Test
+    public void whenNumberIsEvenThanReturnTrue() throws IOException {
+        byte[] number = {1, 4, 3};
+        boolean actualCheckEvenAndOddNumber;
+
+        InputStream inputStream = new ByteArrayInputStream(number);
+
+        EvenAndOddNumber evenAndOddNumber = new EvenAndOddNumber();
+
+        actualCheckEvenAndOddNumber = evenAndOddNumber.isNumber(inputStream);
+
+        assertThat(actualCheckEvenAndOddNumber, is(true));
+    }
+
+    /**
+     * whenNumberIsEvenThanReturnTrue test situation when number is odd.
+     * @throws IOException
+     */
+
+    @Test
+    public void whenNumberIsOddThanReturnFalse() throws IOException {
+        byte[] number = {1, 1, 3};
+        boolean actualCheckEvenAndOddNumber;
+
+        InputStream inputStream = new ByteArrayInputStream(number);
+
+        EvenAndOddNumber evenAndOddNumber = new EvenAndOddNumber();
+
+        actualCheckEvenAndOddNumber = evenAndOddNumber.isNumber(inputStream);
+
+        assertThat(actualCheckEvenAndOddNumber, is(false));
+
+    }
+
+}

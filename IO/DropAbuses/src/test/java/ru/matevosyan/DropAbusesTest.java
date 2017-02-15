7f11597c151fbffb7050f6ad0c3baec74bd94c06
VardanMatevosyan <vardya@yandex.ru>
2017-02-15 14:03:05 +0300

    edit test

--- IO/DropAbuses/src/test/java/ru/matevosyan/DropAbusesTest.java
+++ IO/DropAbuses/src/test/java/ru/matevosyan/DropAbusesTest.java
@@ -1,9 +1,13 @@
 package ru.matevosyan;
 
+
 import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.junit.Test;
 
 import java.io.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  * Created for testing DropAbuses.
@@ -16,7 +20,7 @@ import java.io.*;
 public class DropAbusesTest {
 
     @Test
-    public void whenAddFourWordAndTwoOfThisWordIsAbuseWordThanDeleteTwoAbuseWord() {
+    public void whenAddFourWordAndTwoOfThisWordIsAbuseWordThanDeleteTwoAbuseWord() throws IOException {
 
         String[] abuses = {"abuseWord1", "abuseWord2"};
         String inputString = "abuseWord1 goodWord abuseWord2 goodWord2";
@@ -25,6 +29,23 @@ public class DropAbusesTest {
         InputStream inputStream = new ByteArrayInputStream(inputByte);
         OutputStream outputStream = new ByteOutputStream();
 
+        DropAbuses abusesWord = new DropAbuses();
+
+        abusesWord.dropAbuses(inputStream, outputStream, abuses);
+
+        String[] expectedString = {"goodWord goodWord2"};
+        OutputStream output = new ByteOutputStream();
+        BufferedWriter expectedBufferedWriter = new BufferedWriter( new OutputStreamWriter(output));
+            for (String anExpectedString : expectedString) {
+                if (anExpectedString != null) {
+                    expectedBufferedWriter.write(anExpectedString);
+                }
+            }
+        expectedBufferedWriter.flush();
+        expectedBufferedWriter.close();
+        outputStream.close();
+        inputStream.close();
+        assertThat(expectedBufferedWriter, is(abusesWord.getStream()));
     }
 
 }

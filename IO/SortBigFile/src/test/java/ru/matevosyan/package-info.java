c5502651dc31345ff961d4cf1962051d948c131c
VardanMatevosyan <vardya@yandex.ru>
2017-02-19 13:46:50 +0300

    add package-info

---
+++ IO/SortBigFile/src/test/java/ru/matevosyan/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created for testing sorted file that program sort and write to a new file.
+ * Created on 19.02.2017.
+ * @since 1.0
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
+package ru.matevosyan;

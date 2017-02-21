d122bb50fcc5780f89446db3406913e12ab51a5d
VardanMatevosyan <vardya@yandex.ru>
2017-02-22 00:25:07 +0300

    change sortArray method

--- IO/SortBigFile/src/main/java/ru/matevosyan/SortFile.java
+++ IO/SortBigFile/src/main/java/ru/matevosyan/SortFile.java
@@ -3,7 +3,6 @@ package ru.matevosyan;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.Arrays;
 
 /**
  * Created SortFile for sort big file and write it to a new file.
@@ -20,17 +19,16 @@ public class SortFile {
 
         try (RandomAccessFile toRead = new RandomAccessFile(source, "r");
                 RandomAccessFile toWrite = new RandomAccessFile(dist, "rw")) {
-            int step = 0;
-            int countStart = 0;
+            int StringSize = (int) toRead.length() / 2;
 
-            String[] stringsFirstToRead = new String[10];
-            String[] stringsSecondToRead = new String[10];
+            String[] stringsFirstToRead = new String[StringSize];
+            String[] stringsSecondToRead = new String[StringSize];
             String[] stringsThirdSortedToRead = new String[20];
             while (toRead.getFilePointer() < 20L) {
-                for (int b = 0; b < 10; b++) {
+                for (int b = 0; b < stringsFirstToRead.length; b++) {
                     stringsFirstToRead[b] = toRead.readLine();
                     if (b >= 9) {
-                        for (int c = 0; c < 10; c++) {
+                        for (int c = 0; c < stringsSecondToRead.length; c++) {
                             stringsSecondToRead[c] = toRead.readLine();
                         }
                     }
@@ -38,30 +36,15 @@ public class SortFile {
 
             }
 
-            stringsThirdSortedToRead = sortArray(stringsFirstToRead, stringsSecondToRead);
+            stringsThirdSortedToRead = sortArray(sortOneArray(stringsFirstToRead), sortOneArray(stringsSecondToRead));
            for (int i = 0; i < stringsThirdSortedToRead.length; i++) {
                 String aStringsThirdSortedToRead = stringsThirdSortedToRead[i];
 
                 toWrite.write(aStringsThirdSortedToRead.getBytes());
                String s = System.getProperty("line.separator");
                 toWrite.write(s.getBytes());
-                //toWrite.seek(i);
-
             }
-            //while (toRead.getFilePointer() < 10L) {
 
-
-//            String[] stringsFirstToReadLast = new String[step];
-//            for (int a = 0; a < toRead.length() - step; a++) {
-//                toRead.seek(step);
-//                stringsFirstToReadLast[a] = toRead.readUTF();
-//            }
-//            Arrays.sort(stringsFirstToReadLast);
-//
-//            for (String aStringsFirstToReadLast : stringsFirstToReadLast) {
-//                toWrite.seek(stringsThirdSortedToRead.length + 1);
-//                toWrite.write(aStringsFirstToReadLast.getBytes());
-//            }
 
             } catch (IOException ioe) {
             System.out.println(ioe.getMessage());
@@ -75,51 +58,71 @@ public class SortFile {
             int j = 0;
             int i = 0;
             int k = 0;
-
+        int count = firstStrings.length + secondStrings.length;
+        int countForI = 0;
+        int countForJ = 0;
             int length = firstStrings.length + secondStrings.length;
             String[] stringsThirdToRead = new String[length];
 
-            while (i < firstStrings[i].length() && j < secondStrings[i].length()) {
+            while (count != 0) {
 
-                if (firstStrings[i].length() <= secondStrings[j].length()) {
+                    if (firstStrings[i].length() <= secondStrings[j].length()) {
 
-                    stringsThirdToRead[k] = firstStrings[i];
-                    if (i < firstStrings.length - 1) {
-                        i++;
+
+                        if (countForI != firstStrings.length) {
+                            stringsThirdToRead[k] = firstStrings[i];
+                            countForI++;
+                            if (i < firstStrings.length - 1) {
+                                i++;
+                            }
+
+                        } else {
+                            if (countForJ != firstStrings.length) {
+                                stringsThirdToRead[k] = secondStrings[j];
+                                countForJ++;
+                            }
+                            if (j < secondStrings.length - 1) {
+                                j++;
+
+                            }
+                        }
+
+                    } else {
+
+                        if (j < secondStrings.length) {
+                            stringsThirdToRead[k] = secondStrings[j];
+                        }
+
+                        if (j < secondStrings.length - 1) {
+                            j++;
+
+                        }
                     }
 
-
-                } else {
-
-                    stringsThirdToRead[k] = secondStrings[j];
-                    if (j < secondStrings.length - 1) {
-                        j++;
-                    }
-
-                }
                 if (k < (length - 1)) {
                     k++;
                 }
 
-            }
-            while (i < firstStrings[i].length()) {
-
-                stringsThirdToRead[k] = firstStrings[i];
-
-                i++;
-                k++;
-
-            }
-
-            while (j < secondStrings[j].length()) {
-
-                stringsThirdToRead[k] = secondStrings[j];
-
-                j++;
-                k++;
-
+                count--;
             }
 
             return stringsThirdToRead;
+        }
+
+        public String[] sortOneArray(String[] array) {
+            for (int out = 0; out < array.length; out++) {
+                for(int in = array.length - 1; in > out; in--) {
+
+
+                    if (array[in].length() < array[in - 1].length()) {
+                        String tmp = array[in];
+                        array[in] = array[in - 1];
+                        array[in - 1] = tmp;
+
+                    }
+
+                }
+            }
+            return array;
         }
     }

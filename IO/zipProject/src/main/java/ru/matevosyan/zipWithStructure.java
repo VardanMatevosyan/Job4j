0fcb774c363f35300d569de325efaeb21df52a64
VardanMatevosyan <vardya@yandex.ru>
2017-02-28 07:30:42 +0300

    add writeToZip method

--- IO/zipProject/src/main/java/ru/matevosyan/zipWithStructure.java
+++ IO/zipProject/src/main/java/ru/matevosyan/zipWithStructure.java
@@ -1,9 +1,6 @@
 package ru.matevosyan;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -18,21 +15,34 @@ import java.util.zip.ZipInputStream;
 
 public class zipWithStructure {
 
-    public void zipping(File file, String[] keys) throws IOException {
+    public void zipping(File file, String ... keys) throws IOException {
 
-        try (ZipInputStream zip = new ZipInputStream(new FileInputStream(file)); ZipFile zipFile = new ZipFile(file)) {
+        try (ZipInputStream zip = new ZipInputStream(new FileInputStream(file));
+             ZipFile zipFile = new ZipFile(file)) {
             ZipEntry entry;
             Enumeration entries = zipFile.entries();
             while (entries.hasMoreElements()) {
                 entry = (ZipEntry) entries.nextElement();
                 for (String key : keys) {
                     if (entry.getName().contains("." + key)) {
-
+                        writeToZip(zipFile.getInputStream(entry),
+                                new BufferedOutputStream(new FileOutputStream(
+                                        new File(file.getParent(), entry.getName()))));
+                    } else if (entry.isDirectory()) {
+                        new File(file.getParent(), entry.getName()).mkdirs();
                     }
                 }
             }
         }  catch (FileNotFoundException fnfe) {
             fnfe.getMessage();
+        }
+    }
+
+    public void writeToZip(InputStream in, OutputStream out) throws IOException {
+        byte[] buff = new byte[1024];
+        int length;
+        while ((length = in.read(buff)) != -1) {
+            out.write(buff, 0, length);
         }
     }
 }

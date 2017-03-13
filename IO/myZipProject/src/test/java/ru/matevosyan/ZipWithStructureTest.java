7b7b0363377e0124e8666f5e5fd7a3229dfed5f4
VardanMatevosyan <vardya@yandex.ru>
2017-03-13 17:08:53 +0300

    add few test methods

--- IO/myZipProject/src/test/java/ru/matevosyan/ZipWithStructureTest.java
+++ IO/myZipProject/src/test/java/ru/matevosyan/ZipWithStructureTest.java
@@ -3,7 +3,16 @@ package ru.matevosyan;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  * Created SortFile for testing sort big file and write it to a new file.
@@ -18,8 +27,8 @@ public class ZipWithStructureTest {
     private static String java;
     private static String classFormat;
     private String[] s = new String[]{java, classFormat};
-    private final static String zip_path_name = "D:\\Tracker-1.0-shaded.jar";
-    private final static String FOLDER_PATH_NAME = "D:\\Tracker-1.0-shaded";
+    private final static String zip_path_name = "\\\\Server2008\\общие докумены\\Производственный отдел\\Tracker-1.0-shaded.jar";
+    private final static String FOLDER_PATH_NAME = "\\\\Server2008\\общие докумены\\Производственный отдел\\Tracker-1.0-shaded";
     private static String outZip;
 
     /**
@@ -30,7 +39,7 @@ public class ZipWithStructureTest {
     @BeforeClass
     public static void executeSameVariable() throws IOException {
 
-        outZip = "D:\\TrackerOUT.zip";
+        outZip = "\\\\Server2008\\общие докумены\\Производственный отдел\\TrackerOUT.zip";
         java = "java";
         classFormat = "class";
 
@@ -41,16 +50,114 @@ public class ZipWithStructureTest {
      */
 
     @Test
-    public void whenAddFileAndSortToAnotherFileThanCheckFileSize() {
+    public void whenUnzipThanCheckUnzippedDirectory() {
+        ZipWithStructure zip = new ZipWithStructure(FOLDER_PATH_NAME, s);
+
+        try {
+            zip.unzipping(zip_path_name);
+
+            File file = new File(zip_path_name.substring(0, zip_path_name.lastIndexOf(".")));
+            boolean checkExistDirectory = false;
+
+            if (file.isDirectory() && file.exists()) {
+                checkExistDirectory = true;
+            }
+
+            assertThat(checkExistDirectory, is(true));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+    @Test
+    public void whenZipThanCheckZippedFileExsisting() {
         ZipWithStructure zip = new ZipWithStructure(FOLDER_PATH_NAME, s);
 
         try {
             zip.unzipping(zip_path_name);
             zip.zipping(outZip);
+            File file = new File(outZip);
+            boolean checkExistDirectory = false;
+
+            if (file.isFile() && file.exists() && file.getName().endsWith(".zip")) {
+                checkExistDirectory = true;
+            }
+
+            assertThat(checkExistDirectory, is(true));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+
+
+    }
+
+    @Test
+    public void whenZipThanCheckAllFilesWithKey() {
+        ZipWithStructure zip = new ZipWithStructure(FOLDER_PATH_NAME, s);
+
+        try {
+            zip.unzipping(zip_path_name);
+            zip.zipping(outZip);
+            List<String> list = zip.getListOfFile();
+            boolean checkExistDirectory = false;
+
+            for (String filesPath : list) {
+                for (String value : s) {
+                    if (filesPath.endsWith(value)) {
+                        checkExistDirectory = true;
+                    }
+                }
+            }
+            assertThat(checkExistDirectory, is(true));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    @Test
+    public void whenZipThanCheckAllFilesInZipWithKey() {
+        ZipWithStructure zip = new ZipWithStructure(FOLDER_PATH_NAME, s);
+
+        try {
+            zip.unzipping(zip_path_name);
+            zip.zipping(outZip);
+
+            boolean checkExistDirectory = false;
+            List<String> arrayListResult = new ArrayList<>();
+            List<String> arrayList = zip.getListOfFile();
+
+            try (ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(outZip))) {
+                ZipEntry entry = zipInputStream.getNextEntry();
+
+                while (entry != null) {
+                    String filePathName = entry.getName();
+                    String folder = zip.getFOLDER_PATH_NAME();
+                    int lastIndex = folder.lastIndexOf("\\");
+                    int folderLength = folder.substring(lastIndex + 1, folder.length()).length();
+
+                    arrayListResult.add(filePathName.substring(folderLength + 1, filePathName.length()));
+                    entry = zipInputStream.getNextEntry();
+                }
+
+                for (int i = 0; i < arrayListResult.size(); i++) {
+                    assertThat(arrayList.get(i), is(arrayListResult.get(i)));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 

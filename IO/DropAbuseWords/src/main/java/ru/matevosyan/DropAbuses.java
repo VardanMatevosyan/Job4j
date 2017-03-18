5accc33bd5cdadf9c433e6b268ac8239187dcf67
VardanMatevosyan <vardya@yandex.ru>
2017-03-19 02:23:29 +0300

    replace implementing to the method replaceAll

--- IO/DropAbuseWords/src/main/java/ru/matevosyan/DropAbuses.java
+++ IO/DropAbuseWords/src/main/java/ru/matevosyan/DropAbuses.java
@@ -1,12 +1,12 @@
 package ru.matevosyan;
 
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.IOException;
 
 /**
  * Created DropAbuses for delete abuses.
@@ -20,52 +20,28 @@ public class DropAbuses {
 
     /**
      * DropAbuses created for deleting abuses from String array.
-     * @param in inputStream passing value.
-     * @param out outputStream passing value.
+     *
+     * @param in     inputStream passing value.
+     * @param out    outputStream passing value.
      * @param abuses an array with abuse words inside.
      */
 
     public void dropAbuses(InputStream in, OutputStream out, String[] abuses) {
 
-            try (BufferedReader buff = new BufferedReader(new InputStreamReader(in))) {
-                String s;
-                while ((s = buff.readLine()) != null) {
-                    String[] strings = s.split(" ");
-
-                    for (String abuse : abuses) {
-                        for (int j = 0; j < strings.length; j++) {
-                            if (abuse.equals(strings[j])) {
-                                strings[j] = null;
-                            }
-                        }
-                    }
-
-                    for (int k = 0; k < strings.length; k++) {
-
-                        //if each words in an array is't null than write that word in the OutputStream.
-                        if (strings[k] != null) {
-                            try (BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(out))) {
-
-                                //creating space to put space between words and assign on the end of words,
-                                // to nothing fo cutting it.
-                                String space = " ";
-                                if (k == strings.length - 1) {
-                                    space = "";
-                                }
-
-                                //invoke write method to write an array of words to outputStream,
-                                // and wrap it with bufferedWriter.
-                                bufferedWriter.write(strings[k] + space);
-
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
+        try (BufferedReader buff = new BufferedReader(new InputStreamReader(in, "UTF-8"))) {
+            String s;
+            while ((s = buff.readLine()) != null) {
+                for (String abuseWord : abuses) {
+                    //delete if sentence contain abuse word
+                    s = s.replaceAll(abuseWord, "");
                 }
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
+                //print to the console
+                PrintStream printStream = new PrintStream(out);
+                printStream.print(s);
             }
 
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }

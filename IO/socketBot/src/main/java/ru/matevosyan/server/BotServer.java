d012dea55c7a80b74922b0e91f4f549e41779e53
VardanMatevosyan <vardya@yandex.ru>
2017-03-21 23:30:29 +0300

    add PrintWrite and BufferedReader

--- IO/socketBot/src/main/java/ru/matevosyan/server/BotServer.java
+++ IO/socketBot/src/main/java/ru/matevosyan/server/BotServer.java
@@ -9,22 +9,27 @@ public class BotServer {
             System.out.println("Waiting for a client...");
 
             Socket socket = ss.accept();
-            System.out.println("Got a client :)!");
+            System.out.println("Got a client");
             System.out.println();
 
             try (InputStream sin = socket.getInputStream();
             OutputStream sout = socket.getOutputStream();
 
-            DataInputStream in = new DataInputStream(sin);
-            DataOutputStream out = new DataOutputStream(sout)) {
+//            DataInputStream in = new DataInputStream(sin);
+//            DataOutputStream out = new DataOutputStream(sout)) {
+
+                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
                 String line = null;
 
                 while(true) {
-                    line = in.readUTF();
+//                    line = in.readUTF();
+                    line = in.readLine();
                     System.out.println("The dumb client just sent me this line : " + String.valueOf(line));
                     System.out.println("I'm sending it back...");
-                    out.writeUTF(line);
+//                    out.writeUTF(line);
+                    out.write(line);
                     out.flush();
                     System.out.println("Waiting for the next line...");
                     System.out.println();

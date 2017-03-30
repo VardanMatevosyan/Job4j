c988a74361151f32a52526ff129004248857178f
VardanMatevosyan <vardya@yandex.ru>
2017-03-30 07:20:59 +0300

    change logic

--- IO/socketBot/src/main/java/ru/matevosyan/server/BotServer.java
+++ IO/socketBot/src/main/java/ru/matevosyan/server/BotServer.java
@@ -1,64 +1,67 @@
 package ru.matevosyan.server;
 import java.net.*;
 import java.io.*;
+
 public class BotServer {
-    public static void main(String[] ar)    {
-        int port = 19999;
-        try {
-            ServerSocket ss = new ServerSocket(port);
-            System.out.println("Waiting for a client...");
 
-            Socket socket = ss.accept();
-            System.out.println("Got a client");
-            System.out.println();
+    private Socket socket;
+    private static final int PORT = 19999;
+    private static final String SPACE = System.getProperty("line.separator");
 
-            try (InputStream sin = socket.getInputStream();
-            OutputStream sout = socket.getOutputStream();
+    public BotServer(Socket socket) {
+        this.socket = socket;
+    }
 
-            DataInputStream in = new DataInputStream(sin);
-            DataOutputStream out = new DataOutputStream(sout)) {
+    public static void main(String[] ar) {
+        try (Socket ss = new ServerSocket(PORT).accept()) {
 
-//                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-//                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            System.out.printf("Got a client%s", SPACE);
 
-                String line = "";
+            BotServer botServer = new BotServer(ss);
+            botServer.startServerWork();
 
-                while(!line.equals("Пока")) {
-                    line = in.readUTF();
-//                    line = in.readLine();
-                    System.out.println("The dumb client just sent me this line : " + String.valueOf(line));
-                    System.out.println("I'm sending it back...");
-                    //out.writeUTF(line);
-//                    out.write(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-                    if (line.contains("Hello")) {
-                        out.writeUTF("Hello my friend");
-                    } else if (line.contains("How do you do")) {
-                        out.writeUTF("Thank's, I'm good, and you?");
-                    } else if (line.equals("Which poem did you like?")) {
-                        out.writeUTF("I love \"My Foolish Dog\"");
-                    } else if (line.equals("Can you send me?")) {
+    public void startServerWork() {
 
-                        out.writeUTF("\n\nYes, you are welcome \n" +
-                                "My dog is quite hip\n" +
-                                "Except when he takes a dip\n" +
-                                "He looks like a fool\n" +
-                                "when he jumps in the pool\n" +
-                                "and reminds me of a sinking ship\n" +
-                                "\n");
-                    } else if (line.contains("Пока")) {
-                        line = "Пока";
-                    } else {
-                        out.writeUTF("What do you want. I don't understand you!");
-                    }
-                    out.flush();
-                    System.out.println("Waiting for the next line...");
-                    System.out.println();
+        try (InputStream sin = socket.getInputStream();
+             OutputStream sout = socket.getOutputStream();
+
+//                 DataInputStream in = new DataInputStream(sin);
+//                 DataOutputStream out = new DataOutputStream(sout)) {
+             BufferedReader in = new BufferedReader(new InputStreamReader(sin));
+             PrintWriter out = new PrintWriter(sout, true)) {
+
+            String line;
+
+            do {
+//                    line = in.readUTF();
+                line = in.readLine();
+                System.out.printf("Client ~ %s%s", line, SPACE);
+
+//                    out.writeUTF(serverAnswer(line) + SPACE);
+
+
+                if (line.contains("Hello")) {
+                    out.print("Hello my friend");
+                    out.println();
+//                    out.write("Hello my friend");
+//                    out.println("");
+                } else if(!line.equals("Hello") && !line.equals("Bye")) {
+                    out.print("What do you want. I don't understand you!");
+                    out.println();
+//                    out.write("What do you want. I don't understand you!");
+//                    out.println("");
                 }
-            } catch (IOException ioe) {
-                ioe.getMessage();
-            }
 
-        } catch(Exception x) { x.printStackTrace(); }
+                    out.flush();
+            } while (!line.equals("Bye"));
+
+        } catch (IOException ioe) {
+            ioe.getMessage();
+        }
     }
 }

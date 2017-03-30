9926c7e7448342cc02e0b09043d9b54f5754e45f
VardanMatevosyan <vardya@yandex.ru>
2017-03-31 00:05:52 +0300

    change a little

--- IO/socketBot/src/test/java/ru/matevosyan/client/BotClientTest.java
+++ IO/socketBot/src/test/java/ru/matevosyan/client/BotClientTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.net.Socket;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -22,36 +21,41 @@ public class BotClientTest {
 
     public void whenClientSendThenServerReturn(String input, String expected, String typeIn) throws IOException {
         Socket socket = mock(Socket.class);
-        ByteArrayOutputStream outType = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outType));
+
+//        ByteArrayOutputStream outType = new ByteArrayOutputStream();
+//        System.setOut(new PrintStream(outType));
 
         ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ByteArrayInputStream typeValue = new ByteArrayInputStream(typeIn.getBytes());
 
+        ByteArrayInputStream typeValue = new ByteArrayInputStream(typeIn.getBytes());
         System.setIn(typeValue);
+
         when(socket.getInputStream()).thenReturn(in);
         when(socket.getOutputStream()).thenReturn(out);
 
-
         BotClient botClient = new BotClient(socket);
-
         botClient.startClient();
 
-        assertThat(outType.toString("UTF-8"), is(expected));
+        assertThat(socket.getOutputStream(), is(expected));
     }
 
+
+//    @Test
+//    public void whenSendHelloThenReturnHello() throws IOException {
+//        this.whenClientSendThenServerReturn("Hello my friend",
+//                String.format("Type something to send to Oracle%sSending this line to the server...%s" +
+//                                "Server ~ Hello my friend%sSending this line to the server...%sServer ~ null%s",
+//                        LN, LN, LN, LN, LN),
+//                String.format("Hello%sBye", LN));
+//    }
+    @Test
+    public void whenSendByThenReturnEmpty() throws IOException {
+        this.whenClientSendThenServerReturn("Hello my friend", "Hello my friend", String.format("Hello%sBye", LN));
+    }
 
     @Test
     public void whenSendHelloThenReturnHello() throws IOException {
-        this.whenClientSendThenServerReturn("Hello my friend",
-                String.format("Type something to send to Oracle%sSending this line to the server...%s" +
-                                "Server ~ Hello my friend%sSending this line to the server...%sServer ~ null%s",
-                        LN, LN, LN, LN, LN),
-                String.format("Hello%sBye", LN));
+
     }
-//    @Test
-//    public void whenSendByThenReturnEmpty() throws IOException {
-//        this.whenClientSendThenServerReturn("", "", "Bye");
-//    }
 }

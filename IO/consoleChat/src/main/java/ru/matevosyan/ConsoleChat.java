5008aae9cffd153a8f7c6a332d09164048765ae2
VardanMatevosyan <vardya@yandex.ru>
2017-04-15 22:36:23 +0300

    refactoring

--- IO/consoleChat/src/main/java/ru/matevosyan/ConsoleChat.java
+++ IO/consoleChat/src/main/java/ru/matevosyan/ConsoleChat.java
@@ -1,6 +1,14 @@
 package ru.matevosyan;
 
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -22,9 +30,17 @@ public class ConsoleChat {
     private File file;
     private String outPutFile;
 
-    private String LN = System.getProperty("line.separator");
+    private static final String LN = System.getProperty("line.separator");
 
+    private static final String EXIT = "EXIT";
+    private static final String SAMESTUFF = "CONTINUE";
+    private static final String STOP = "STOP";
 
+    /**
+     * Constructor fo console chat.
+     * @param fileName file that using to take a word from it and send to user.
+     * @param outFileName file use to writing console chat.
+     */
 
     public ConsoleChat(final File fileName, final String outFileName) {
 
@@ -33,20 +49,22 @@ public class ConsoleChat {
 
     }
 
-    public void readUserData() {
+    /**
+     * major method using to read answer from user, after go to a file and read from file a random word to send back.
+     * And when user tale it answer from file he also can answer again while he send "Bye" and at the end all dialog
+     * write to the file.
+     */
 
-        String exit = "EXIT";
-        String sameStuff = "CONTINUE";
-        String stop = "STOP";
+    public void manageUserData() {
 
         String readUser;
 
         do {
 
                 readUser = scanner.nextLine();
-            if (!(readUser.equals(exit)) && !(readUser.equals(stop))) {
+            if (!(EXIT.equals(readUser)) && !(STOP.equals(readUser))) {
 
-                if (readUser.equals(sameStuff)) {
+                if (SAMESTUFF.equals(readUser)) {
                     readUser = scanner.nextLine();
                 }
 
@@ -54,30 +72,34 @@ public class ConsoleChat {
 
             }
 
-            if (readUser.equals(stop)) {
+            if (STOP.equals(readUser)) {
                 boolean exitFromWhile = false;
-                while (!(readUser.equals(sameStuff))) {
+                while (!(SAMESTUFF.equals(readUser))) {
                     writeFile(this.outPutFile, readUser);
-                    if (!(readUser.equals(exit))) {
+                    if (!(EXIT.equals(readUser))) {
                         readUser = scanner.nextLine();
                         writeFile(this.outPutFile, readUser);
-                        if (readUser.equals(exit)) {
-                            readUser = sameStuff;
+                        if (EXIT.equals(readUser)) {
+                            readUser = SAMESTUFF;
                             exitFromWhile = true;
                             writeFile(this.outPutFile, readUser);
                         }
                     }
-
                 }
 
                 if (exitFromWhile) {
-                    readUser = exit;
+                    readUser = EXIT;
                 }
 
             }
-        } while (!readUser.equals(exit));
+        } while (!EXIT.equals(readUser));
         writeFile(this.outPutFile, readUser);
     }
+
+    /**
+     * Use to read answer from file and print to user, after send to {@link ConsoleChat#writeFile(String, String...)}.
+     * @param readUser using to send user answer and send to {@link ConsoleChat#writeFile(String, String...)}
+     */
 
     public void readFile(String readUser) {
         try (BufferedReader fin = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
@@ -97,6 +119,12 @@ public class ConsoleChat {
             ioe.getMessage();
         }
     }
+
+    /**
+     * Use to write to the file everything in the console chat.
+     * @param file use to be writen.
+     * @param readAndAnswerUser all stuff to write to the file.
+     */
 
     public void writeFile(String file, String ... readAndAnswerUser) {
 
@@ -118,22 +146,6 @@ public class ConsoleChat {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-//    public static void main(String[] args) {
-//        String pathSource = "C:\\Users\\Admin\\Desktop\\pathSource.txt";
-//        String outPutPathSource = "C:\\Users\\Admin\\Desktop\\outPutPathSourceFile.txt";
-//
-//        File sourceFile = new File(pathSource);
-//        File outPutPathSourceFile = new File(outPutPathSource);
-//
-//        String pathSource = "pathSource.txt";
-//        String outPutPathSource = "outPutPathSourceFile.txt";
-//
-//        ConsoleChat consoleChat = new ConsoleChat(pathSource, outPutPathSource);
-//        consoleChat.readUserData();
-//
-//
-//    }
 }

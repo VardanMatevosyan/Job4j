54bbfc4805af215819afbade7bae9561199a799e
VardanMatevosyan <vardya@yandex.ru>
2017-04-19 07:16:25 +0300

    refactoring it is work

--- IO/socketBot/src/main/java/ru/matevosyan/server/BotServer.java
+++ IO/socketBot/src/main/java/ru/matevosyan/server/BotServer.java
@@ -1,6 +1,22 @@
 package ru.matevosyan.server;
-import java.net.*;
-import java.io.*;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created BotServer to interchange with client.
+ * Created on 19.03.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
 
 public class BotServer {
 
@@ -8,11 +24,22 @@ public class BotServer {
     private static final int PORT = 19999;
     private static final String SPACE = System.getProperty("line.separator");
 
+    /**
+     * Constructor with passing socket but in server side send ServerSocket.
+     * @param socket socket that represent input ans output stream in network connection.
+     */
+
     public BotServer(Socket socket) {
         this.socket = socket;
     }
 
-    public static void main(String[] ar) {
+    /**
+     * Main method that initialise variable and start the program with Server socket and when client will be connect.
+     * @param args without parameter don't work main method.
+     */
+
+
+    public static void main(String[] args) {
         try (Socket ss = new ServerSocket(PORT).accept()) {
 
             System.out.printf("Got a client%s", SPACE);
@@ -25,39 +52,34 @@ public class BotServer {
         }
     }
 
+    /**
+     * Method startServerWork was created to send and receive data to the client using ServerSocket connection.
+     */
+
     public void startServerWork() {
 
         try (InputStream sin = socket.getInputStream();
              OutputStream sout = socket.getOutputStream();
 
-//                 DataInputStream in = new DataInputStream(sin);
-//                 DataOutputStream out = new DataOutputStream(sout)) {
              BufferedReader in = new BufferedReader(new InputStreamReader(sin));
              PrintWriter out = new PrintWriter(sout, true)) {
 
             String line;
 
             do {
-//                    line = in.readUTF();
+
                 line = in.readLine();
                 System.out.printf("Client ~ %s%s", line, SPACE);
-
-//                    out.writeUTF(serverAnswer(line) + SPACE);
-
 
                 if (line.contains("Hello")) {
                     out.print("Hello my friend");
                     out.println();
-//                    out.write("Hello my friend");
-//                    out.println("");
-                } else if(!line.equals("Hello") && !line.equals("Bye")) {
+                } else if (!line.equals("Hello") && !line.equals("Bye")) {
                     out.print("What do you want. I don't understand you!");
                     out.println();
-//                    out.write("What do you want. I don't understand you!");
-//                    out.println("");
                 }
 
-                    out.flush();
+                out.flush();
             } while (!line.equals("Bye"));
 
         } catch (IOException ioe) {

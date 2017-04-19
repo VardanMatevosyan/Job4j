22bd0b95d4783423093746c669d7b7d6a929f9e3
VardanMatevosyan <vardya@yandex.ru>
2017-04-19 07:17:45 +0300

    refactoring

--- IO/socketBot/src/test/java/ru/matevosyan/server/BotServerTest.java
+++ IO/socketBot/src/test/java/ru/matevosyan/server/BotServerTest.java
@@ -13,7 +13,10 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
- * Created by Admin on 25.03.2017.
+ * Created BotClientTest to test BotClient class.
+ * Created on 25.03.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
  */
 
 public class BotServerTest {
@@ -21,6 +24,12 @@ public class BotServerTest {
     private static final String LN = System.getProperty("line.separator");
     private static final String EMPTYSTRING = System.getProperty("line.separator");
 
+    /**
+     * Method whenClientAskThenServerAnswer was created for check correct output and expected value.
+     * @param input passing variable that server must read.
+     * @param expected value that will be check with input variable.
+     * @throws IOException to handle invoking  input and output exception.
+     */
 
     private void whenClientAskThenServerAnswer(String input, String expected) throws IOException {
         Socket socket = mock(Socket.class);
@@ -38,12 +47,26 @@ public class BotServerTest {
 
     }
 
+    /**
+     * whenClientAskForExitThenServerAnswerEmpty was created to test server responding value to client.
+     * invoking {@link BotServerTest#whenClientAskThenServerAnswer(String, String)} and passing client input value.
+     * and expected value.
+     * @throws IOException to handle invoking  input and output exception.
+     */
+
     @Test
     public void whenClientAskForExitThenServerAnswerEmpty() throws IOException {
 
         this.whenClientAskThenServerAnswer("Bye", "");
 
     }
+
+    /**
+     * whenClientAskForExitThenServerAnswerEmpty was created to test server responding value to client.
+     * invoking {@link BotServerTest#whenClientAskThenServerAnswer(String, String)} and passing client input value.
+     * and expected value.
+     * @throws IOException to handle invoking  input and output exception.
+     */
 
     @Test
     public void whenClientAskForBlaBlaThenServerAnswerDoNotUnderstand() throws IOException {
@@ -53,12 +76,19 @@ public class BotServerTest {
 
     }
 
+    /**
+     * whenClientAskForExitThenServerAnswerEmpty was created to test server responding value to client.
+     * invoking {@link BotServerTest#whenClientAskThenServerAnswer(String, String)} and passing client input value.
+     * and expected value.
+     * @throws IOException to handle invoking  input and output exception.
+     */
+
     @Test
     public void whenClientAskForBlaBlaTwoTimesThenServerAnswerDoNotUnderstandTwoTimes() throws IOException {
 
         this.whenClientAskThenServerAnswer(String.format("sdfsdsdfsdfsdf%sBsdssdsdsdsdy%sBye", LN, LN),
-                "What do you want. I don't understand you!" + EMPTYSTRING +
-                        "What do you want. I don't understand you!" + EMPTYSTRING);
+                "What do you want. I don't understand you!" + EMPTYSTRING
+                        + "What do you want. I don't understand you!" + EMPTYSTRING);
 
     }
 

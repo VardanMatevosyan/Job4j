d5d96d23f7be29edbe6b703f7afa08262b86d832
VardanMatevosyan <vardya@yandex.ru>
2017-04-19 07:15:27 +0300

    refactoring it is work

--- IO/socketBot/src/main/java/ru/matevosyan/client/BotClient.java
+++ IO/socketBot/src/main/java/ru/matevosyan/client/BotClient.java
@@ -1,65 +1,91 @@
 package ru.matevosyan.client;
 
-import java.net.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.net.Socket;
+
+/**
+ * Created BotClient to interchange with server.
+ * Created on 19.03.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
 
 public class BotClient {
 
-    private static final int PORT = 19999;
-    private static final String ADDRESS = "127.0.0.1";
     private Socket socket;
     private static final String SPACE = System.getProperty("line.separator");
+    private boolean exit = true;
+
+    /**
+     * Constructor with passing socket (for better testing with mockito).
+     * @param socket socket that represent input ans output stream in network connection.
+     */
 
     public BotClient(final Socket socket) {
         this.socket = socket;
     }
 
-    public BotClient() {
-
-    }
+    /**
+     * Main method that initialise variable and start the program.
+     * @param args without parameter don't work main method.
+     */
 
     public static void main(String[] args) {
-            BotClient botClient = new BotClient();
-            botClient.startClient();
+        Socket socket = new Socket();
+        BotClient botClient = new BotClient(socket);
+        botClient.startClient();
 
     }
+
+    /**
+     * Method startClient was created to send and receive data to the server using socket connection.
+     */
 
     public void startClient() {
 
         try {
-            InetAddress ipAddress = InetAddress.getByName(ADDRESS);
-            socket = new Socket(ipAddress, PORT);
-//            System.out.printf("Yes! I just got hold of the program. %s", SPACE);
 
             try (
 
-            InputStream sin = socket.getInputStream();
-            OutputStream sout = socket.getOutputStream();
+                    InputStream sin = socket.getInputStream();
+                    OutputStream sout = socket.getOutputStream();
 
-//            DataInputStream in = new DataInputStream(sin);
-//            DataOutputStream out = new DataOutputStream(sout);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(sin));
+                    PrintWriter out = new PrintWriter(sout, true);
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(sin));
-            PrintWriter out = new PrintWriter(sout, true);
+                    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in))) {
+                    String line;
+                    System.out.printf("Type something to send to Oracle%s", SPACE);
 
-            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in))) {
-                String line;
-                System.out.printf("Type something to send to Oracle%s", SPACE);
+                    while (this.exit) {
 
-                do {
-                    line = keyboard.readLine();
-                    System.out.printf("Sending this line to the server...%s", SPACE);
-                    out.println(line);
+                        line = keyboard.readLine();
+                        System.out.printf("Sending this line to the server...%s", SPACE);
+                        out.println(line);
 
-                    line = in.readLine();
-                    System.out.printf("Server ~ %s", line + SPACE);
+                        if (!(line.equals("Bye"))) {
 
-                } while (!line.equals("Bye"));
-            } catch (IOException ioe) {
-            ioe.getMessage();
+                            line = in.readLine();
+                            System.out.printf("Server ~ %s", line + SPACE);
+                        } else {
+                            this.exit = false;
+                        }
+
+                    }
+
+            } catch (IOException x) {
+                x.printStackTrace();
             }
             socket.close();
-        } catch (Exception x) {
+
+        } catch (IOException x) {
             x.printStackTrace();
         }
     }

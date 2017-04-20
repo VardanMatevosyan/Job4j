383f1e097ded39120b62e0e5962769f95e47e23c
VardanMatevosyan <vardya@yandex.ru>
2017-04-20 11:36:16 +0300

    change adding and removing method

--- CollectionsLite/efficiency/src/main/java/ru/matevosyan/EfficiencyList.java
+++ CollectionsLite/efficiency/src/main/java/ru/matevosyan/EfficiencyList.java
@@ -10,9 +10,8 @@ public class EfficiencyList {
 
     public long add(Collection<String> collection, String line, int amount) {
         this.efficiencyTime = System.currentTimeMillis();
-
         for(int i = 0; i < amount; i++) {
-            collection.add(line);
+            collection.add(line + i);
         }
         return this.efficiencyTime = System.currentTimeMillis() - this.efficiencyTime;
     }
@@ -20,12 +19,14 @@ public class EfficiencyList {
         this.efficiencyTime = System.currentTimeMillis();
 
         Iterator<String> iterator = collection.iterator();
-//        while(iterator.hasNext()) {
-            for (int i = 0; i < amount; i++) {
-                iterator.next();
-                iterator.remove();
-            }
-//        }
+
+        int i = 0;
+        while(iterator.hasNext() && (i < amount)) {
+
+            iterator.next();
+            iterator.remove();
+            i++;
+        }
         return this.efficiencyTime = System.currentTimeMillis() - this.efficiencyTime;
     }
 
@@ -37,9 +38,15 @@ public class EfficiencyList {
 
         EfficiencyList efficiencyList = new EfficiencyList();
 
+        //-------------------------------------------------------------
+
         System.out.printf("ArrayList%s", LN);
-        System.out.print(String.format("Add time --- %s%sRemove time --- %s%s", efficiencyList.add(arrayList, "Array", 55), LN,
-                efficiencyList.delete(arrayList, 50), LN));
+
+        long timeAddArray = efficiencyList.add(arrayList, "Linked", 1_000_005);
+        long timeRemoveArray = efficiencyList.delete(arrayList, 1_000_000);
+
+        System.out.print(String.format("Add time --- %s%sRemove time --- %s%s",
+                timeAddArray, LN, timeRemoveArray, LN));
         int i = 0;
         for (String str : arrayList) {
 
@@ -47,9 +54,15 @@ public class EfficiencyList {
         }
         System.out.printf("Amount --- %s%s%s", i, LN, LN);
 
+        //-------------------------------------------------------------
+
         System.out.printf("LinkedList%s", LN);
-        System.out.print(String.format("Add time --- %s%sRemove time --- %s%s",efficiencyList.add(linkedList, "Linked", 55), LN,
-                efficiencyList.delete(linkedList, 50), LN));
+
+        long timeAddLinkedList = efficiencyList.add(linkedList, "Linked", 1_000_005);
+        long timeRemoveLinkedList = efficiencyList.delete(linkedList, 1_000_000);
+
+        System.out.print(String.format("Add time --- %s%sRemove time --- %s%s", timeAddLinkedList, LN,
+                timeRemoveLinkedList, LN));
 
         i = 0;
         for (String str : linkedList) {
@@ -58,9 +71,15 @@ public class EfficiencyList {
         }
         System.out.printf("Amount --- %s%s%s", i, LN, LN);
 
+        //---------------------------------------------------------------
+
         System.out.printf("TreeSet%s", LN);
-        System.out.print(String.format("Add time --- %s%sRemove time  --- %s%s", efficiencyList.add(treeSet, "Tree", 55), LN,
-                efficiencyList.delete(treeSet, 50), LN));
+
+        long timeAddTeeSet = efficiencyList.add(treeSet, "treeSet", 1_000_005);
+        long timeRemoveTreeSet = efficiencyList.delete(treeSet, 1_000_000);
+
+        System.out.printf(String.format("Add time --- %s%sRemove time  --- %s%s", timeAddTeeSet, LN,
+                timeRemoveTreeSet, LN));
         i = 0;
         for (String str : treeSet) {
 

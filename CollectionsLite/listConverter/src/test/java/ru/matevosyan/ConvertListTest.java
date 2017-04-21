38a7872b5af5d71b87283237ad6539572ab62f1e
VardanMatevosyan <vardya@yandex.ru>
2017-04-21 07:13:24 +0300

    add ConvertListTest

---
+++ CollectionsLite/listConverter/src/test/java/ru/matevosyan/ConvertListTest.java
@@ -0,0 +1,28 @@
+package ru.matevosyan;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.ArrayList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
+public class ConvertListTest {
+
+    @Test
+    public void whenAddThousendAndFiveToTreeSetThanReturnFive() {
+
+        //assign
+       
+        //act
+     
+        //assert
+        assertThat(expectedTreeSet, is(2_000_000));
+    }
+   
+}

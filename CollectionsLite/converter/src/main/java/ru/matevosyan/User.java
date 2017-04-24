daa29378719d3f9042d74d7413cfcd90a4d53554
VardanMatevosyan <vardya@yandex.ru>
2017-04-24 06:39:14 +0300

    add User class

---
+++ CollectionsLite/converter/src/main/java/ru/matevosyan/User.java
@@ -0,0 +1,28 @@
+package ru.matevosyan;
+
+public class User {
+
+    private final int id;
+
+    private final String name;
+
+    private final String city;
+
+    public User(int id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}

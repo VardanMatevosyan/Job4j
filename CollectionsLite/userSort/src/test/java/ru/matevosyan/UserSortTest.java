b2e588ca953874d6dcaee4644c64f6b53d24745d
VardanMatevosyan <vardya@yandex.ru>
2017-04-26 07:35:16 +0300

    aad UserSortTest class to userSort

---
+++ CollectionsLite/userSort/src/test/java/ru/matevosyan/UserSortTest.java
@@ -0,0 +1,55 @@
+package ru.matevosyan;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class UserSortTest {
+
+    @Test
+    public void whenPassArrayThanCheckList() {
+
+        //assign
+        User admin = new User(1, "Tom", "LA");
+        User firsUser = new User(2, "Vova", "Moscow");
+        User secondUser = new User(3, "Vardan", "Dzhanfida");
+
+        List<User> userList = new ArrayList<>();
+        HashMap<Integer, User> actualUserMap;
+
+        UserConverter convertList = new UserConverter();
+
+        //act
+        userList.add(admin);
+        userList.add(firsUser);
+        userList.add(secondUser);
+
+        actualUserMap = convertList.process(userList);
+
+
+        Iterator<Map.Entry<Integer, User>> hashMap = actualUserMap.entrySet().iterator();
+        Iterator<User> userValues = userList.iterator();
+
+        //assert
+        while (hashMap.hasNext() && userValues.hasNext()) {
+
+            Map.Entry<Integer, User> userEntry = hashMap.next();
+            User userArrayList = userValues.next();
+
+            assertThat(userEntry.getKey(), is(userArrayList.getId()));
+            assertThat(userEntry.getValue().getName(), is(userArrayList.getName()));
+
+        }
+
+    }
+
+
+
+}

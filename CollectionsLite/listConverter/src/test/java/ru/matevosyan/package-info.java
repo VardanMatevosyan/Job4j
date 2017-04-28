ec51d01436c2fd12da05b4363582611c3c8c1a0a
VardanMatevosyan <vardya@yandex.ru>
2017-04-28 14:50:40 +0300

    add packege-info

---
+++ CollectionsLite/listConverter/src/test/java/ru/matevosyan/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created to test {@link ru.matevosyan.ConvertList} class.
+ * that convert from array to list and backward.
+ * Created on 28.04.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+package ru.matevosyan;

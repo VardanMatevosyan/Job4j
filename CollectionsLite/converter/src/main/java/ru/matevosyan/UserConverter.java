4f6df3f947dc53025410fdd498511ec6d3b4be71
VardanMatevosyan <vardya@yandex.ru>
2017-04-28 13:37:38 +0300

    add javadoc

--- CollectionsLite/converter/src/main/java/ru/matevosyan/UserConverter.java
+++ CollectionsLite/converter/src/main/java/ru/matevosyan/UserConverter.java
@@ -1,9 +1,24 @@
 package ru.matevosyan;
 
-import java.util.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Iterator;
+
+/**
+ * Created UserConverter to convert list to map.
+ * Created on 28.04.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
 
 public class UserConverter {
 
+    /**
+     * process method was created to convert array list to map.
+     * @param list list of users.
+     * @return user map.
+     */
 
     public HashMap<Integer, User> process(List<User> list) {
 

f40d4e72fe825f0ea41877798bdaab884950a716
VardanMatevosyan <vardya@yandex.ru>
2017-04-28 14:01:30 +0300

    add javadoc

--- CollectionsLite/efficiency/src/main/java/ru/matevosyan/EfficiencyList.java
+++ CollectionsLite/efficiency/src/main/java/ru/matevosyan/EfficiencyList.java
@@ -1,14 +1,27 @@
 package ru.matevosyan;
 
-
 import java.util.Collection;
 import java.util.Iterator;
+
+
+/**
+ * EfficiencyList was created to see how list and set have works.
+ * Created on 28.04.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
 
 public class EfficiencyList {
 
     private long efficiencyTime;
 
-
+    /**
+     * add method adding amount times string value to a collection<String></>.
+     * @param collection passing to the add method to be filled by lines.
+     * @param line string value that adding to a collection with iterable value i.
+     * @param amount it's mean how many times line would be added to the collection.
+     * @return times in milliseconds, it is mean how long collection fills up.
+     */
 
     public long add(Collection<String> collection, String line, int amount) {
         this.efficiencyTime = System.currentTimeMillis();
@@ -17,6 +30,14 @@ public class EfficiencyList {
         }
         return this.efficiencyTime = System.currentTimeMillis() - this.efficiencyTime;
     }
+
+    /**
+     * delete method deleting amount times string value from a collection<String></>.
+     * @param collection use to remove lines from the collection that passed to method.
+     * @param amount it's mean how many times line would be added to the collection.
+     * @return times in milliseconds.
+     */
+
     public long delete(Collection<String> collection, int amount) {
         this.efficiencyTime = System.currentTimeMillis();
 

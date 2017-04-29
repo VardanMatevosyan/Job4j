c102b3640ec2d77484bb1fba29637251622ccbe0
VardanMatevosyan <vardya@yandex.ru>
2017-04-29 20:06:20 +0300

    add javadoc to User.java in bank project

--- CollectionsLite/bank/src/main/java/ru/matevosyan/User.java
+++ CollectionsLite/bank/src/main/java/ru/matevosyan/User.java
@@ -1,20 +1,76 @@
 package ru.matevosyan;
 
+/**
+ * User class for bank project.
+ * Created on 28.04.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
 public class User {
 
     private final String name;
+    private final String passport;
+
+    /**
+     * User class constructor.
+     * @param name User name.
+     * @param passport User passport.
+     */
+
+    public User(String name, String passport) {
+        this.name = name;
+        this.passport = passport;
+    }
+
+    /**
+     * Getter to get User name.
+     * @return User name.
+     */
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Getter to get User passport.
+     * @return User passport.
+     */
+
+    public String getPassport() {
+        return passport;
+    }
+
+    /**
+     * Override equals method {@link Object#equals(Object)}.
+     * @param o passing object to check that all object value is the same.
+     * @return boolean values: if equals true, else false.
+     */
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         User user = (User) o;
 
-        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null) {
+            return false;
+        }
+
         return passport != null ? passport.equals(user.passport) : user.passport == null;
 
     }
+
+    /**
+     * Override hashCode method to compute hash value to identify  object.
+     * @return object's hash code.
+     */
 
     @Override
     public int hashCode() {
@@ -23,19 +79,5 @@ public class User {
         return result;
     }
 
-    private final String passport;
-
-    public User(String name, String passport) {
-        this.name = name;
-        this.passport = passport;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassport() {
-        return passport;
-    }
 
 }

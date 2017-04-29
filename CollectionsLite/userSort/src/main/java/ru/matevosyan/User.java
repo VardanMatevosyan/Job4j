7b00097dd63037b0d01168e6e2ac36ef16459e76
VardanMatevosyan <vardya@yandex.ru>
2017-04-29 19:33:27 +0300

    add javadoc to User.java

--- CollectionsLite/userSort/src/main/java/ru/matevosyan/User.java
+++ CollectionsLite/userSort/src/main/java/ru/matevosyan/User.java
@@ -1,23 +1,51 @@
 package ru.matevosyan;
 
+/**
+ * User class.
+ * Created on 28.04.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
 public class User implements Comparable<User> {
 
     private final String name;
-
     private final int age;
+
+    /**
+     * User class constructor.
+     * @param name User name.
+     * @param age User age.
+     */
 
     public User(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
+    /**
+     * Getter to get User name.
+     * @return User name.
+     */
+
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter to get User age.
+     * @return User name.
+     */
+
     public int getAge() {
         return age;
     }
+
+    /**
+     * Override compareTo(User user) method to compare users by age.
+     * @param user is User object.
+     * @return int value.
+     */
 
     @Override
     public int compareTo(User user) {

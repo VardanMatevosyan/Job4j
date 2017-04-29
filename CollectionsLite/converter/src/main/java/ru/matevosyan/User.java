9fde9813b4a439e1600c079be1e6efcf80363780
VardanMatevosyan <vardya@yandex.ru>
2017-04-29 19:42:24 +0300

    add javadoc to User.java

--- CollectionsLite/converter/src/main/java/ru/matevosyan/User.java
+++ CollectionsLite/converter/src/main/java/ru/matevosyan/User.java
@@ -1,12 +1,25 @@
 package ru.matevosyan;
 
+
+/**
+ * User class for converter project.
+ * Created on 28.04.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
 public class User {
 
     private final int id;
-
     private final String name;
-
     private final String city;
+
+    /**
+     * User class constructor.
+     * @param id User id.
+     * @param name User name.
+     * @param city User city.
+     */
 
     public User(int id, String name, String city) {
         this.id = id;
@@ -14,15 +27,31 @@ public class User {
         this.city = city;
     }
 
+    /**
+     * Getter to get User id.
+     * @return User id.
+     */
+
     public int getId() {
         return id;
     }
+
+    /**
+     * Getter to get User name.
+     * @return User name.
+     */
 
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter to get User city.
+     * @return User city.
+     */
+
     public String getCity() {
         return city;
     }
+
 }

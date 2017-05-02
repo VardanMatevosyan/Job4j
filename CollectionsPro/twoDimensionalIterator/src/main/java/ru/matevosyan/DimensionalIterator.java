c1ee80c1bcdd6ce08f01e5a3691e4d96c1265257
VardanMatevosyan <vardya@yandex.ru>
2017-05-02 16:45:13 +0300

    add implementation

--- CollectionsPro/twoDimensionalIterator/src/main/java/ru/matevosyan/DimensionalIterator.java
+++ CollectionsPro/twoDimensionalIterator/src/main/java/ru/matevosyan/DimensionalIterator.java
@@ -1,5 +1,7 @@
 package ru.matevosyan;
 
+import java.util.Objects;
+
 /**
  * TwoDementionIterator class.
  * Created on 28.04.2017.
@@ -7,7 +9,24 @@ package ru.matevosyan;
  * @version 1.0
  */
 
-public class DimensionalIterator {
+public class DimensionalIterator implements ArrayIterator {
+
+    private int indexX = 0;
+    private int indexY = 0;
+    private final int[][] array;
+
+    public DimensionalIterator(int[][] array) {
+        this.array = array;
+    }
+
+    @Override
+    public int next() {
+        return array[indexX++][indexY++];
+    }
 
 
+    @Override
+    public boolean hasNext() {
+        return array.length > indexX && array.length > indexY;
+    }
 }

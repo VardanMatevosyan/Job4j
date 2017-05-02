87e0fd112317ada411496305a7f5764edd13254f
VardanMatevosyan <vardya@yandex.ru>
2017-05-02 07:39:06 +0300

    TwoDimensionalIterator.java rename to DimensionalIterator.java

---
+++ CollectionsPro/twoDimensionalIterator/src/main/java/ru/matevosyan/DimensionalIterator.java
@@ -0,0 +1,13 @@
+package ru.matevosyan;
+
+/**
+ * TwoDementionIterator class.
+ * Created on 28.04.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
+public class DimensionalIterator {
+
+
+}

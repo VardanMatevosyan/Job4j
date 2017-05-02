06cb92318d9870291fa1f81f9cf2cd6d63d1c9b4
VardanMatevosyan <vardya@yandex.ru>
2017-05-02 16:43:35 +0300

    addinterface ArrayIterator

---
+++ CollectionsPro/twoDimensionalIterator/src/main/java/ru/matevosyan/ArrayIterator.java
@@ -0,0 +1,11 @@
+package ru.matevosyan;
+
+import java.util.Objects;
+
+/**
+ * Created by Пользователь on 02.05.2017.
+ */
+public interface ArrayIterator {
+    int next();
+    boolean hasNext();
+}

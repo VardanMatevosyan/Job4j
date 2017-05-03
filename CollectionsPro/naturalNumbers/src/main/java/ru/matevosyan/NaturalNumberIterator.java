43d64f79ab34da1687fed45a24887645e3dd8113
VardanMatevosyan <vardya@yandex.ru>
2017-05-03 13:00:11 +0300

    add NaturalNumberIterator to the naturalNumbers

---
+++ CollectionsPro/naturalNumbers/src/main/java/ru/matevosyan/NaturalNumberIterator.java
@@ -0,0 +1,26 @@
+package ru.matevosyan;
+
+/**
+ * NaturalNumberIterator class.
+ * Created on 28.04.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
+public interface NaturalNumberIterator {
+
+
+    /**
+     * Created next() method to iterate each element from array.
+     * @return  arrays element value.
+     */
+
+    int next();
+
+    /**
+     * Created hasNext() method to check if any of elements are in array to read, looking to next() pointer.
+     * @return true if there are any value to read, else false.
+     */
+
+    boolean hasNext();
+}

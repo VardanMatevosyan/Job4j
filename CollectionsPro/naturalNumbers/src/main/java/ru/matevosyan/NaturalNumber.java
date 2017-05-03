5e7375581965819a789009d7bd60c0a95690b8ed
VardanMatevosyan <vardya@yandex.ru>
2017-05-03 12:59:40 +0300

    add NaturalNumber to the naturalNumbers

---
+++ CollectionsPro/naturalNumbers/src/main/java/ru/matevosyan/NaturalNumber.java
@@ -0,0 +1,53 @@
+package ru.matevosyan;
+
+/**
+ * EvenNumber class.
+ * Created on 03.05.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
+public class NaturalNumber implements ArrayIterator {
+
+    private int index = 0;
+    private int j = 0;
+    private final int[] array;
+
+    /**
+     * EvenNumber constructor.
+     * @param array an array.
+     */
+
+    public NaturalNumber (int[] array) {
+        this.array = array;
+    }
+
+    /**
+     * Override the next() method to iterate each element from an array.
+     * @return even numbers from an array.
+     */
+
+    @Override
+    public int next() {
+        int value = 0;
+        for (int i = index++; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                value = i;
+                break;
+            } else {
+                index++;
+            }
+        }
+        return array[value];
+    }
+
+    /**
+     * Override hasNext() method to check if eny of elements are in array to read, looking to next() pointer.
+     * @return true if there are any elements to read, else return false.
+     */
+
+    @Override
+    public boolean hasNext() {
+        return array.length > index;
+    }
+}

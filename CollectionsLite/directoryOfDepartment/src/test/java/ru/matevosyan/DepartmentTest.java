5c8c992465d2882f1c623ff11638fe39138f4519
VardanMatevosyan <vardya@yandex.ru>
2017-05-11 23:52:21 +0300

    change DepartmentTest.java it is work

--- CollectionsLite/directoryOfDepartment/src/test/java/ru/matevosyan/DepartmentTest.java
+++ CollectionsLite/directoryOfDepartment/src/test/java/ru/matevosyan/DepartmentTest.java
@@ -2,36 +2,37 @@ package ru.matevosyan;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.Comparator;
 import java.util.TreeSet;
+import java.util.List;
+import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 /**
- * Created by Admin on 07.05.2017.
+ * DepartmentTest class for testing ascending and descending sort.
+ * Created on 11.05.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
  */
+
 public class DepartmentTest {
+
+    /**
+     * whenPassDepartmentSerThanCheckSortedDepartment() was created to test ascend sorting for {@link Department#name}.
+     */
+
     @Test
     public void whenPassDepartmentSerThanCheckSortedDepartment() {
 
         //assign
         TreeSet<String> depCode = new TreeSet<>();
-        Department department = new Department(depCode);
-
-//        TreeSet<String> code = new TreeSet<>();
-//        DepartmentCode departmentCode = new DepartmentCode(code);
-
         List<String> codeExpected = new ArrayList<>();
-        Iterator iterator = department.getName().iterator();
+        Department department = new Department(depCode);
         int i = 0;
 
         //act
-//        code.add("K1");
-//        code.add("K2");
-
         depCode.add("K1\\SK1");
         depCode.add("K1\\SK2");
         depCode.add("K1\\SK1\\SSK1");
@@ -49,26 +50,35 @@ public class DepartmentTest {
         codeExpected.add("K2\\SK1\\SSK1");
         codeExpected.add("K2\\SK1\\SSK2");
 
-//        depCode.addAll(departmentCode.getCode());
+        depCode.addAll(department.checkDepartmentCode());
 
         //assert
-        while (iterator.hasNext()) {
-            assertThat(iterator.next(), is(codeExpected.get(i++)));
+        for (String actualDepartmentCode : department.getName()) {
+            assertThat(actualDepartmentCode, is(codeExpected.get(i++)));
         }
     }
+
+    /**
+     * whenPassDepartmentSerThanCheckDescendingSortedDepartment() was created to test descend sorting.
+     * for {@link Department#name}.
+     */
 
     @Test
     public void whenPassDepartmentSerThanCheckDescendingSortedDepartment() {
         //assign
-        TreeSet<String> depCode = new TreeSet<>();
-        Department department = new Department(depCode);
+        TreeSet<String> depCode = new TreeSet<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        });
 
         List<String> codeExpected = new ArrayList<>();
-        Iterator iterator = department.getName().iterator();
+        Department department = new Department(depCode);
+
         int i = 0;
 
         //act
-
         depCode.add("K1\\SK1");
         depCode.add("K1\\SK2");
         depCode.add("K1\\SK1\\SSK1");
@@ -87,12 +97,11 @@ public class DepartmentTest {
         codeExpected.add("K1\\SK1");
         codeExpected.add("K1");
 
-
-
+        depCode.addAll(department.checkDepartmentCode());
         //assert
-        while (iterator.hasNext()) {
-            department.descendingSort();
-            assertThat(iterator.next(), is(codeExpected.get(i++)));
+
+        for (String actualDepartmentCode : depCode) {
+            assertThat(actualDepartmentCode, is(codeExpected.get(i++)));
         }
     }
 }

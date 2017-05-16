311441e49bbd2e742879072c3eb119a1fc072cd6
VardanMatevosyan <vardya@yandex.ru>
2017-05-16 07:29:36 +0300

    add UserStore.java

---
+++ CollectionsPro/generics/simpleStore/src/main/java/ru/matevosyan/UserStore.java
@@ -0,0 +1,11 @@
+package ru.matevosyan;
+
+/**
+ * Created by Admin on 16.05.2017.
+ */
+public class UserStore extends MajorStore {
+
+    public UserStore(SimpleArray<Base> userSimpleArray) {
+        super(userSimpleArray);
+    }
+}

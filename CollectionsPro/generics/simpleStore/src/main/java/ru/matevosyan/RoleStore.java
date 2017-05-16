76aeedacf4c16bda22516d1d848e68732aa8c090
VardanMatevosyan <vardya@yandex.ru>
2017-05-16 07:28:28 +0300

    add RoleStore.java

---
+++ CollectionsPro/generics/simpleStore/src/main/java/ru/matevosyan/RoleStore.java
@@ -0,0 +1,11 @@
+package ru.matevosyan;
+
+/**
+ * Created by Admin on 16.05.2017.
+ */
+public class RoleStore extends MajorStore {
+
+    public RoleStore(SimpleArray<Base> storage) {
+        super(storage);
+    }
+}

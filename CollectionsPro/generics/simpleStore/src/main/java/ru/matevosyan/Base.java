f9d0e8a8211b8ef42fa467baca774a694094cb98
VardanMatevosyan <vardya@yandex.ru>
2017-05-17 06:43:20 +0300

    add Base.java

--- CollectionsPro/generics/simpleStore/src/main/java/ru/matevosyan/Base.java
+++ CollectionsPro/generics/simpleStore/src/main/java/ru/matevosyan/Base.java
@@ -1,16 +1,33 @@
 package ru.matevosyan;
 
 /**
- * Created by Admin on 16.05.2017.
+ * abstract class Base.
+ * Created on 16.05.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
  */
 
 public abstract class Base {
 
+    /**
+     * Base id.
+     */
+
     private String id;
+
+    /**
+     * Getter for id field.
+     * @return base id.
+     */
 
     String getId() {
         return this.id;
     }
+
+    /**
+     * Setter for id field.
+     * @param id base id.
+     */
 
     void setId(String id) {
         this.id = id;

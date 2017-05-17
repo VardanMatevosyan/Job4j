12151c0aff5062e018a0c73dc00fa07181829c18
VardanMatevosyan <vardya@yandex.ru>
2017-05-17 07:09:31 +0300

    add Store.java

--- CollectionsPro/generics/simpleStore/src/main/java/ru/matevosyan/Store.java
+++ CollectionsPro/generics/simpleStore/src/main/java/ru/matevosyan/Store.java
@@ -1,15 +1,43 @@
 package ru.matevosyan;
 
 /**
- * Created by Admin on 16.05.2017.
+ * interface Store<T extends Base>.
+ * Created on 16.05.2017.
+ * @author Matevosyan Vardan.
+ * @version 1.0.
  */
+
 public interface Store<T extends Base> {
+
+    /**
+     * Method add() added value to {@link SimpleArray#object}.
+     * @param value is all Base class child value.
+     */
 
     void add(T value);
 
+    /**
+     * Method update(String id, T second) update value in {@link SimpleArray#object}.
+     * @param id is values id in {@link SimpleArray#object}.
+     * @param second is all Base class child value that going to be added instead of value with id that passing.
+     * to this method.
+     */
+
     void update(String id, T second);
 
-    void delete(String id);
+    /**
+     * Method delete(String id) delete value from {@link SimpleArray#object}.
+     * @param id is values id in {@link SimpleArray#object} that going to be deleted.
+     * @return true if value wad deleted.
+     */
+
+    boolean delete(String id);
+
+    /**
+     * Method get(String id) return value from {@link SimpleArray#object}.
+     * @param id is values id in {@link SimpleArray#object} that going to be return.
+     * @return is value from {@link SimpleArray#object}.
+     */
 
     T get(String id);
 }

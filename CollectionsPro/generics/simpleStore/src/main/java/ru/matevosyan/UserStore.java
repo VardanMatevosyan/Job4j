8c1535c02d6ad93fdd370c9f2f34d571e1cd4a1b
VardanMatevosyan <vardya@yandex.ru>
2017-05-17 06:56:45 +0300

    add UserStore.java

--- CollectionsPro/generics/simpleStore/src/main/java/ru/matevosyan/UserStore.java
+++ CollectionsPro/generics/simpleStore/src/main/java/ru/matevosyan/UserStore.java
@@ -1,9 +1,18 @@
 package ru.matevosyan;
 
 /**
- * Created by Admin on 16.05.2017.
+ * UserStore class to store the user.
+ * Created on 16.05.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
  */
+
 public class UserStore extends MajorStore {
+
+    /**
+     * Constructor to invoke parent constructor.
+     * @param userSimpleArray SimpleArray storage.
+     */
 
     public UserStore(SimpleArray<Base> userSimpleArray) {
         super(userSimpleArray);

c54b17591d4b53eb3241602b865f6327c60dd4a2
VardanMatevosyan <vardya@yandex.ru>
2017-05-21 11:31:58 +0300

    add javadoc to IDynamicArray.java

--- CollectionsPro/list/dynamicArray/src/main/java/ru/matevosyan/IDynamicArray.java
+++ CollectionsPro/list/dynamicArray/src/main/java/ru/matevosyan/IDynamicArray.java
@@ -1,10 +1,26 @@
 package ru.matevosyan;
 
 /**
- * Created by Admin on 18.05.2017.
+ * IDynamicArray interface.
+ * Created on 18.05.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
  */
+
 public interface IDynamicArray<E> {
+
+    /**
+     * Add value to an array.
+     * @param value is value that type which you declare in generics.
+     */
+
     void add(E value);
+
+    /**
+     * Get value from an array by index passing.
+     * @param index position value in an array.
+     * @return value that type specify in generic.
+     */
 
     E get(int index);
 }

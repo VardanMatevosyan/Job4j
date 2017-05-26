06ed048991210c93599409d7945b4468cc07b9c0
VardanMatevosyan <vardya@yandex.ru>
2017-05-26 14:21:31 +0300

    add one method

--- CollectionsPro/list/cycleDetection/src/main/java/ru/matevosyan/CycleDetection.java
+++ CollectionsPro/list/cycleDetection/src/main/java/ru/matevosyan/CycleDetection.java
@@ -10,21 +10,46 @@ import java.util.NoSuchElementException;
  * @version 1.0
  */
 
-public class CycleDetection<E>{
+public class CycleDetection<E> {
 
-    private Node<E> last;
     private int size = 0;
-    private Node<E> first;
+
 
     /**
      * Constructor.
      */
 
     public CycleDetection() {
-
     }
 
+    boolean hasCycle(Node<E> first) {
+        Node<E> fast;
+        Node<E> slow;
+        boolean detForFastToSlow = true;
 
+        if (first != null) {
+            fast = first;
+            slow = first;
+        } else {
+            return false;
+        }
+
+        while (fast != null) {
+            if (detForFastToSlow) {
+                fast = fast.next.next;
+            } else {
+                fast = fast.next;
+            }
+
+            if (fast == slow.next) {
+                detForFastToSlow = false;
+                if (fast == slow) {
+                    slow = first;
+                }
+            }
+        }
+        return detForFastToSlow;
+    }
     /**
      * Class Node describe linkedList entry.
      * @param <E> parameter that defined when create an instance of a class.

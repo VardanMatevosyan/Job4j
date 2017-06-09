4e9cf9b9cfd114c1c72ea71735829a4d4af4b4aa
VardanMatevosyan <vardya@yandex.ru>
2017-06-09 21:24:15 +0300

    add User.java

--- CollectionsPro/map/user/src/main/java/ru/matevosyan/User.java
+++ CollectionsPro/map/user/src/main/java/ru/matevosyan/User.java
@@ -16,39 +16,9 @@ public class User {
     private int children;
     private Calendar birthday;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        User user = (User) o;
-
-        if (children != user.children) {
-            return false;
-        }
-        if (name != null ? !name.equals(user.name) : user.name != null) {
-            return false;
-        }
-        return birthday != null ? birthday.equals(user.birthday) : user.birthday == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + children;
-        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
-        return result;
-    }
-
     /**
      * Constructor.
      */
-
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;

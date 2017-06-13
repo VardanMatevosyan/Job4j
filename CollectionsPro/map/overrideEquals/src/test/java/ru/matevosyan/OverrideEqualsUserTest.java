e2b27df151ad79545d263b2dadfde8a312b587cc
VardanMatevosyan <vardya@yandex.ru>
2017-06-13 07:06:51 +0300

    add OverrideEqualsUserTest

---
+++ CollectionsPro/map/overrideEquals/src/test/java/ru/matevosyan/OverrideEqualsUserTest.java
@@ -0,0 +1,36 @@
+package ru.matevosyan;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+
+/**
+ * Was created to test OverrideEqualsUserTest class.
+ * Created on 07.05.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
+
+public class OverrideEqualsUserTest {
+    @Test
+    public void whenCreateTwoSameObjectThanCheckTheResultOfComparing() {
+        Calendar c = new GregorianCalendar(2017, Calendar.JUNE, 8);
+        OverrideEqualsUser first = new OverrideEqualsUser("first", 3, c);
+        OverrideEqualsUser second = new OverrideEqualsUser("first", 3, c);
+        Map<OverrideEqualsUser, Object> map = new HashMap<>();
+
+        map.put(first, "User");
+        map.put(second, "User");
+
+        System.out.println(map);
+        assertThat(first.equals(second), is(false));
+    }
+}

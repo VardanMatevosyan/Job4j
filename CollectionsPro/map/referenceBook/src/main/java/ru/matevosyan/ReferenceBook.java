da8878077119188a6037c23853280f1588fb7b49
VardanMatevosyan <vardya@yandex.ru>
2017-06-23 07:14:59 +0300

    add ReferenceBook.java

---
+++ CollectionsPro/map/referenceBook/src/main/java/ru/matevosyan/ReferenceBook.java
@@ -0,0 +1,15 @@
+package ru.matevosyan;
+
+/**
+ * ReferenceBook class.
+ * Created on 23.06.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
+public class ReferenceBook {
+
+    /**
+     * Constructor.
+     *
+}

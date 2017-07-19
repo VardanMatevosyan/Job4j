5e21deb2d88d708509b554772d7646b218c9b351
VardanMatevosyan <vardya@yandex.ru>
2017-07-20 00:40:05 +0300

    Task 1. Create tree structure [#1711] 1. Создать элементарную структуру дерева [#1711]

---
+++ CollectionsPro/tree/simpleTree/src/main/java/ru/matevosyan/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * For SimpleTree.
+ * Created on 02.07.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+
+package ru.matevosyan;
---
+++ CollectionsPro/tree/simpleTree/src/test/java/ru/matevosyan/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Was created to test SimpleTree class.
+ * Created on 02.07.2017.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ */
+package ru.matevosyan;

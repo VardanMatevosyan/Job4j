00d76027e13cd3fb2b90935a43b0ab2954139b43
VardanMatevosyan <vardya@yandex.ru>
2017-10-07 21:48:19 +0300

    7.Multithreading. 3.Monitor. 4.Поиск текста в файле.[#1106]
    second commit. 7. Multithreading . 3. Monitor, Synchronized . 4. Поиск текста в файле. [#1106]
    
    7.Multithreading.  3.Monitor. 4. Поиск текста в файле.
    
    7.Multithreading, 3.Monitor, 4. Поиск текста в файле. [#1106]
    
    7.Multithreading, 3.Monitor, 4. Поиск текста в файле. [#1106]

---
+++ Multithreading/synchronized/src/main/java/ru/matevosyan/ParallelSearch.java
@@ -0,0 +1,92 @@
+package ru.matevosyan;
+
+import java.io.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * ParallelSearch class for quick file searching in the file system.
+ * @author Matevosyan Vardan
+ * @version 1.0
+ * created on 03.10.2017
+ */
+
+public class ParallelSearch extends Thread {
+
+    private final String text;
+    private volatile Queue<String> listOfFindFileNames;
+    private CustomSynchQueue<File> customSynchQueue;
+
+    /**
+     * Getter for return list of find file names.
+     * @return list of files.
+     */
+
+    public Queue<String> getListOfFindFileNames() {
+        return listOfFindFileNames;
+    }
+
+    /**
+     * Constructor for ParallelSearch.
+     * root - directory from which start searching files.
+     * @param text - word to find out.
+     * exts - to find exactly files with passing extension.
+     */
+
+    public ParallelSearch(String text, CustomSynchQueue<File> customSynchQueue) {
+        this.text = text;
+        listOfFindFileNames = new PriorityQueue<>();
+        this.customSynchQueue = customSynchQueue;
+    }
+
+    /**
+     * run the searchText method while taking files from the queue is not empty.
+     * if it take empty object wich is end of the queue than put it bake.
+     * to tell other threads that it is the end of queue for stop processing.
+     */
+
+    @Override
+    public void run() {
+        boolean isDone = false;
+        while (!isDone) {
+            File fileForSearching;
+
+                fileForSearching =  customSynchQueue.removeFromQueue();
+                if (fileForSearching == FileOrganizer.EMPTY_OBJECT) {
+                    customSynchQueue.addToQueue(fileForSearching);
+                    isDone = true;
+                } else {
+                    this.searchText(fileForSearching);
+                }
+        }
+    }
+
+    /**
+     * Search text in the files.
+     * And after seatchText method add all files to listOfFindFileNames queue.
+     * @param currentFile is file where searching.
+     */
+
+    public Queue<String> searchText(File currentFile) {
+            String textInTheFile;
+            try (LineNumberReader lineNumberReader = new LineNumberReader(
+                    new BufferedReader(new FileReader(currentFile)))) {
+                while ((textInTheFile = lineNumberReader.readLine()) != null) {
+                    if (textInTheFile.contains(this.text)) {
+                        listOfFindFileNames.add(currentFile.getAbsolutePath());
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(currentFile.getAbsolutePath());
+                    }
+                }
+            } catch (IOException e) {
+                e.getMessage();
+            }
+
+            return listOfFindFileNames;
+    }
+
+}

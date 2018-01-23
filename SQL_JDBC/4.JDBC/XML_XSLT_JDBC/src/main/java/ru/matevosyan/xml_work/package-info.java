cd35f57fe38820cf2986ad93dbbbb399a56b8815
VardanMatevosyan <vardya@yandex.ru>
2018-01-23 14:08:58 +0300

    remove

--- SQL_JDBC/4.JDBC/XML_XSLT_JDBC/src/main/java/ru/matevosyan/xml_work/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Work (parsing and get data from database) with xml files.
- * Created on 01.01.2018
- * @since 1.0
- * @author Matevosyan Vardan.
- * @version 1.0
- */
-
-package ru.matevosyan.xml_work;

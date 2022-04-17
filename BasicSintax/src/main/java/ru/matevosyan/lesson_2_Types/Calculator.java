ae728f4de1153aab6de07ea5dbc4f2987a7ad9ac
VardanMatevosyan <vardanmt@gmail.com>
2022-04-18 00:40:16 +0300

    fix codecove link

--- BasicSintax/src/main/java/ru/matevosyan/lesson_2_Types/Calculator.java
+++ BasicSintax/src/main/java/ru/matevosyan/lesson_2_Types/Calculator.java
@@ -4,36 +4,26 @@ public class Calculator {
 
 	private double result;
 
-	
 	public double add(double first, double second){
-		
 		this.result = first + second;
 		return this.result;
 				
 	}
 
 	public double substruct(double first, double second){
-			
 		this.result = first - second;
 		return this.result;
-		
-						
 	}
 
 	public double div(double first, double second){
-				
 		this.result = first / second;
 		return this.result;
 		
 	}
 
 	public double multiple(double first, double second){
-				
 		this.result = first * second;
 		return this.result;
 						
 	}
-
-	
-
 }
